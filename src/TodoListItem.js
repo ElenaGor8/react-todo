5fe0c93f7eeddeefe7704b94a55084eadaf11b43
ElenaGor8 <elena.gornovoy@gmail.com>
2023-06-13 14:03:30 -0400

    Rubric made style changes to color, fonts, buttons

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { ReactComponent as Remove } from "./img/delete_FILL0_wght400_GRAD0_opsz48.svg";
 import style from './TodoListItem.module.css';
 
 const TodoListItem = ({ todo, onRemoveTodo }) => {
     return (
         <li className={style.ListItem}>
             {todo.title}
-            <button type="button" onClick={() => onRemoveTodo(todo.id)}>Remove</button>
+            <button className={style.ButtonRemove}
+            type="button"
+                onClick={() => onRemoveTodo(todo.id)}>
+                <Remove/>
+            </button>
         </li>
     );
 };
 
-export default TodoListItem;
+export default TodoListItem;        
+
